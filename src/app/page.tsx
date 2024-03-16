cf8821e5c3fda78b93c92f62b6787cfc273ff6d6
passion studio <passionsteven28@gmail.com>
2024-03-16 12:55:23 +0300

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      hi
+      <div>
+        <h1>Truly an online file converter</h1>
+        <p>
+          This is a simple file converter that is built with Next.js and ffmpeg.
+        </p>
+      </div>
     </main>
   );
 }
